c0277b7f7fb6800b43895b495fcb5adf9e5d6a3f
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-02-20 03:46:44 +0530

    Add interestial add

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/handler/JokeHandler.java
@@ -0,0 +1,28 @@
+package com.udacity.gradle.builditbigger.handler;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.amrendra.displaylibrary.JokeDisplayActivity;
+import com.udacity.gradle.builditbigger.task.Debug;
+import com.udacity.gradle.builditbigger.task.FetchJokeTask;
+
+/**
+ * Created by Amrendra Kumar on 19/02/16.
+ */
+public class JokeHandler implements FetchJokeTask.JokeListener {
+
+    Context mContext;
+
+    public JokeHandler(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public void jokeLoaded(String joke) {
+        Debug.i("Joke Loaded : " + joke, false);
+        Intent intent = new Intent(mContext, JokeDisplayActivity.class);
+        intent.putExtra(JokeDisplayActivity.JOKE_DISPLAY_INTENT, joke);
+        mContext.startActivity(intent);
+    }
+}
