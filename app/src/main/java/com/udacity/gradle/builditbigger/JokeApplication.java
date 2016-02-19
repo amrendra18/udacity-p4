12900161c5099861abf3d1b89288839e5351e556
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-02-20 01:39:53 +0530

    Add adv to free & remove adv from paid flavor

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeApplication.java
@@ -0,0 +1,14 @@
+package com.udacity.gradle.builditbigger;
+
+import android.app.Application;
+
+/**
+ * Created by Amrendra Kumar on 20/02/16.
+ */
+public class JokeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
