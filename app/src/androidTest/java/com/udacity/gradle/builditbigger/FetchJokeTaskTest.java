12a85013f612de04e80438195eedf9ae65a08e96
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-02-23 02:19:39 +0530

    Update test case

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokeTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/FetchJokeTaskTest.java
@@ -4,37 +4,23 @@ import android.test.AndroidTestCase;
 
 import com.udacity.gradle.builditbigger.task.FetchJokeTask;
 
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Amrendra Kumar on 19/02/16.
  */
-public class FetchJokeTaskTest extends AndroidTestCase implements FetchJokeTask.JokeListener {
-    /*
-    How to test async tasks
-    http://stackoverflow.com/questions/2321829/android-asynctask-testing-with-android-test-framework
-     */
-
-    // create  a signal to let us know when our task is done.
-    final CountDownLatch signal = new CountDownLatch(1);
+public class FetchJokeTaskTest extends AndroidTestCase {
 
     public void testJokeIsRetrieved() {
-        new FetchJokeTask(this).execute();
+        String joke = null;
         try {
-            boolean pass = signal.await(20, TimeUnit.SECONDS);
-            assertTrue("Timed Out", pass);
-        } catch (InterruptedException e) {
+            joke = new FetchJokeTask().execute().get();
+        } catch (InterruptedException | ExecutionException e) {
             fail("Error : " + e.getLocalizedMessage());
         }
-    }
-
-    @Override
-    public void jokeLoaded(String joke) {
         assertNotNull(joke);
         assertTrue(joke.trim().length() > 0);
         String error = "Unable to resolve host";
         assertTrue("Network Error", !joke.contains(error));
-        signal.countDown();
     }
 }
